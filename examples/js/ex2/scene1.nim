102be07831a195d7b90ea36ab915dd208e0b6967
Double_oxygeN <mail@double-oxygen.net>
2020-03-25 16:33:22 +0900

    :wrench: use macro

--- examples/js/ex2/scene1.nim
+++ examples/js/ex2/scene1.nim
@@ -1,57 +1,14 @@
 import scenefw
 import ../components/canvascomp
 
-type
-  Scene1* = ref object of BaseScene
-    counter: Natural
+scene Scene1, CanvasComp:
+  var counter: Natural
 
-proc newScene1*: Scene1 =
-  Scene1(counter: 0)
+  start:
+    self.counter = 0
 
+  update:
+    inc self.counter
 
-method visitInit(self: Component; scene: Scene1) {.base, tags: [IOEffect].} =
-  discard
-
-
-proc init(self: Scene1; component: CanvasComp) {.tags: [IOEffect].} =
-  self.counter = 0
-
-
-method visitInit(self: CanvasComp; scene: Scene1) =
-  scene.init(self)
-
-
-method init(self: Scene1; component: Component) =
-  component.visitInit(self)
-
-
-method visitUpdate(self: Component; scene: Scene1) {.base, tags: [IOEffect].} =
-  discard
-
-
-proc update(self: Scene1; component: CanvasComp) {.tags: [IOEffect].} =
-  inc self.counter
-
-
-method visitUpdate(self: CanvasComp; scene: Scene1) =
-  scene.update(self)
-
-
-method update(self: Scene1; component: Component) =
-  component.visitUpdate(self)
-
-
-method visitDraw(self: Component; scene: Scene1) {.base, tags: [IOEffect, WaqwaDrawEffect].} =
-  discard
-
-
-proc draw(self: Scene1; component: CanvasComp) {.tags: [IOEffect, WaqwaDrawEffect].} =
-  component.fillColor("hsl(" & $self.counter & " 80% 60%)")
-
-
-method visitDraw(self: CanvasComp; scene: Scene1) =
-  scene.draw(self)
-
-
-method draw(self: Scene1; component: Component) =
-  component.visitDraw(self)
+  draw:
+    component.fillColor "hsl(" & $self.counter & " 80% 60%)"
